9c8026801e710636d26060225cb111d0695a332d
Giovanni Garcia <giovanniantonygarcia@gmail.com>
2024-03-24 00:32:25 -0300

    jsx

--- src/components/page/ItemListContainer/ItemList.jsx
+++ src/components/page/ItemListContainer/ItemList.jsx
@@ -1,5 +1,5 @@
 // import { AgregarDocs } from "../../../AgregarDocs";
-import ProductCard from "../../common/productCard/productCard";
+import ProductCard from "../../common/productCard/ProductCard.jsx";
 
 const ItemList = ({ products }) => {
   return (
