a62a3515a9b62bb98ce422cd72ece45d1aba976d
Giovanni Garcia <giovanniantonygarcia@gmail.com>
2024-06-04 21:13:39 -0300

    navigation bar repair

--- src/components/layout/NavBar/NavBar.jsx
+++ src/components/layout/NavBar/NavBar.jsx
@@ -1,23 +1,92 @@
-import React from "react";
+// import React, { useState } from "react";
+// import "./NavBar.css";
+// import Cartwidget from "../../common/Cartwidget/Cartwidget";
+// import { Link } from "react-router-dom";
+
+// const NavBar = () => {
+//   const [showMenu, setShowMenu] = useState(false);
+
+//   const handleMenuClick = () => {
+//     setShowMenu(!showMenu);
+//   };
+//   return (
+//     <header>
+//       <div className="titulo-logo">
+//         <img src="/logo-tipo.svg" alt="logo-vidrialum" id="logoTipo" />
+//         <h1>VIDRIALUM</h1>
+//       </div>
+//       <nav>
+//         <div id="menuIcon" onClick={handleMenuClick}>
+//           &#9776;
+//         </div>
+//         <ul className={`list-nav ${showMenu ? "show-menu" : ""}`}>
+//           <li>
+//             <Link to="/">Inicio</Link>
+//           </li>
+//           <li>
+//             <a>Categorias</a>
+//             <ul className="desplegable">
+//               <li>
+//                 <Link to={"/category/ventanas"}>Ventanas</Link>
+//               </li>
+//               <li>
+//                 <Link to={"/category/puertas"}>Puertas</Link>
+//               </li>
+//             </ul>
+//           </li>
+//           <li>
+//             <a href="#">Contacto</a>
+//           </li>
+//         </ul>
+//       </nav>
+//       <Cartwidget />
+//     </header>
+//   );
+// };
+
+// export default NavBar;
+
+import React, { useState } from "react";
 import "./NavBar.css";
 import Cartwidget from "../../common/Cartwidget/Cartwidget";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+    // Close the dropdown menu when the main menu is toggled
+    if (showMenu) setShowDropdown(false);
+  };
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header>
-      <div className="titulo-logo">
-        <img src="/logo-tipo.svg" alt="logo-vidrialum" id="logoTipo" />
-        <h1>VIDRIALUM</h1>
-      </div>
+      <Link to="/">
+        <div className="titulo-logo">
+          <img src="/logo-tipo.svg" alt="logo-vidrialum" id="logoTipo" />
+          <h1>VIDRIALUM</h1>
+        </div>
+      </Link>
+
       <nav>
-        <ul className="lista-nav">
+        <div id="menuIcon" onClick={handleMenuClick}>
+          &#9776;
+        </div>
+        <ul className={`list-nav ${showMenu ? "show-menu" : ""}`}>
           <li>
             <Link to="/">Inicio</Link>
           </li>
           <li>
-            <a>Categorias</a>
-            <ul className="desplegable">
+            <a onClick={handleDropdownClick}>Categorias</a>
+            <ul
+              className={`desplegable ${showDropdown ? "show-dropdown" : ""}`}
+            >
               <li>
                 <Link to={"/category/ventanas"}>Ventanas</Link>
               </li>
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT,
+//   storageBucket: import.meta.env.VITE_STORAGE,
+//   messagingSenderId: import.meta.env.VITE_MESSAGIN,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT,
-  storageBucket: import.meta.env.VITE_STORAGE,
-  messagingSenderId: import.meta.env.VITE_MESSAGIN,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyC0ie3IaE2vgtuUAt4goQ5WNua9CNFDJf8",
+  authDomain: "vidrialum-2024.firebaseapp.com",
+  projectId: "vidrialum-2024",
+  storageBucket: "vidrialum-2024.appspot.com",
+  messagingSenderId: "1066075209445",
+  appId: "1:1066075209445:web:b1a83735ff9a362962de0c",
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
